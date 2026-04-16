6e4bebd4f717c25f6180bd8584a1313e5c99a897
ProtonnF <215946562+ProtonnF@users.noreply.github.com>
2026-04-15 22:52:19 -0400

    Add files via upload

--- test.js
+++ test.js
@@ -1,20 +1,22 @@
 const axios = require("axios");
 
 async function runTest() {
-    const studentEmail = "hani0024@algonquinlive.com";
-    const testName = "getAll to show all product";
-    const url ="https://your-project-name.onrender.com/products";
+  const studentEmail = "hani0024@algonquinlive.com";
+  const testName = "getAll to show all product";
+  const url = "https://fullstack-project-tugq.onrender.com/products";
 
-    try {
-        const response = await axios.get(url);
-        if (response.status === 200) {
-            console.log(`${studentEmail} - ${testName} - 200 - PASSED`);
-        } else {
-            console.log(`${studentEmail} - ${testName} - ${response.status} - FAILED`);
-        }
-    } catch (error) {
-        console.log(`${studentEmail} - ${testName} - 500 - FAILED`);
+  try {
+    const response = await axios.get(url);
+    if (response.status === 200) {
+      console.log(`${studentEmail} - ${testName} - 200 - PASSED`);
+    } else {
+      console.log(
+        `${studentEmail} - ${testName} - ${response.status} - FAILED`,
+      );
     }
+  } catch (error) {
+    console.log(`${studentEmail} - ${testName} - 500 - FAILED`);
+  }
 }
 
 runTest();
